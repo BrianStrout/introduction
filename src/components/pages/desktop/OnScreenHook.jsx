03450402f64e5a0ac055c0a97490227f6af5cdb7
Brian J. Strout <brianjstrout@gmail.com>
2022-12-03 23:42:10 -0500

    onscreen hook

---
+++ src/components/pages/desktop/OnScreenHook.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState, useRef, useMemo, useEffect } from "react";
+
+const OnScreenHook = (options, targetRef) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const callBackFunction = (entries) => {
+    const [entry] = entries; //const entry= entries[0]
+    setIsVisible(entry.isIntersecting);
+  };
+
+  //   options;
+  const optionsMemo = useMemo(() => {
+    return options;
+  }, [options]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callBackFunction, options);
+    const currentTarget = targetRef.current;
+    if (currentTarget) observer.observe(currentTarget);
+    return () => {
+      if (currentTarget) observer.unobserve(currentTarget);
+    };
+  }, [targetRef, options]);
+
+  return <h1>fillerer</h1>;
+};
+
+export default OnScreenHook;
